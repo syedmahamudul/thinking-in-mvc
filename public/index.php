ae53ce6a4e8b03633def2572d53edd6b2facdc8c
mahmud <syedmahamudhassan@gmail.com>
2026-03-07 16:28:55 +0600

    update gitignor file

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Src\Core\Route;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/../routes/web.php';
+Route::resolve();
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Src\Core\Route;
 
 Route::get('/', 'HomeController@index');
+// Route::get('/user', 'UserController@index');
